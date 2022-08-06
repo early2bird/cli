1c4ebd8db40ac38e4d7bdeb3deb4ae4e6a9952d3
nestops <qabcdef9@163.com>
2022-08-06 16:13:32 +0800

    初始化

---
+++ index.ts
@@ -0,0 +1,14 @@
+import {Command} from 'commander';
+
+const program = new Command();
+
+program
+    .name('string-util')
+    .description('CLI to some JavaScript string utilities')
+    .version('0.8.0');
+program
+    .option('-d, --debug', 'output extra debugging')
+
+program.parse(process.argv);
+const options = program.opts();
+console.log(options, '参数');
