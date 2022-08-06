0c8ae30d74d8133cc6efaa662f04b1cfd542ec3c
nestops <qabcdef9@163.com>
2022-08-06 20:27:52 +0800

    降低eslint版本为7.xx

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
-import { Command } from "commander";
+import { Command } from 'commander';
 
 const program = new Command();
 
-program
-  .name("string-util")
-  .description("CLI to some JavaScript string utilities")
-  .version("0.8.0");
-program.option("-d, --debug", "output extra debugging");
+program.name('string-util').description('CLI to some JavaScript string utilities').version('0.8.0');
+program.option('-d, --debug', 'output extra debugging');
 
 program.parse(process.argv);
 const options = program.opts();
-console.log(options, "参数");
+console.log(options, '参数');
